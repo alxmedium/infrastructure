520cddcff4d5714c80c312883b3df78984aab8c1
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2020-02-16 16:20:17 -0600

    Add alxmedium_circleci_iam_user with access to the VPC and IAM resources
    Add read only access to the IAM resource to the alxmedium_circleci_iam_user
    
    Add full network access to the VPC resource to thr alxmedium_circleci_iam_user
    
    Resolve issue in the CircleCI configuration to run the plan job

--- main.tf
+++ main.tf
@@ -6,10 +6,83 @@ variable "aws_secret_access_key" {
   type = string
 }
 
-
 provider "aws" {
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
   region     = "us-east-1"
   version    = "2.47.0"
+}
+
+resource "aws_iam_user" "alxmedium_circleci_iam_user" {
+  name = "alxmedium_circleci_iam_user"
+  path = "/"
+}
+
+resource "aws_iam_access_key" "alxmedium_circleci_iam_access_key" {
+  user = aws_iam_user.alxmedium_circleci_iam_user.name
+}
+
+resource "aws_iam_user_policy" "alxmedium_circleci_iam_user_policy" {
+  name   = "alxmedium_circleci_iam_user_policy"
+  user   = aws_iam_user.alxmedium_circleci_iam_user.name
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iam:GenerateCredentialReport",
+                "iam:GenerateServiceLastAccessedDetails",
+                "iam:Get*",
+                "iam:List*",
+                "iam:SimulateCustomPolicy",
+                "iam:SimulatePrincipalPolicy"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeRouteTables",
+                "ec2:CreateRoute",
+                "ec2:DeleteRoute",
+                "ec2:ReplaceRoute"
+            ],
+            "Resource": [
+                "*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeNetworkInterfaces",
+                "ec2:CreateNetworkInterface",
+                "ec2:DeleteNetworkInterface",
+                "ec2:CreateNetworkInterfacePermission",
+                "ec2:DeleteNetworkInterfacePermission",
+                "ec2:DescribeNetworkInterfacePermissions",
+                "ec2:ModifyNetworkInterfaceAttribute",
+                "ec2:DescribeNetworkInterfaceAttribute",
+                "ec2:DescribeAvailabilityZones",
+                "ec2:DescribeVpcs",
+                "ec2:DescribeSubnets"
+            ],
+            "Resource": [
+                "*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:AssignPrivateIpAddresses",
+                "ec2:UnassignPrivateIpAddresses"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+}
+  EOF
 }
