e940fe01f6a8a473d0994c452469a3c381a1ce96
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2020-02-25 19:12:09 -0600

    Resolve security issues
    Remove alxmedium_circleci_iam_user
    
    Remove the production workflow and the apply job from the CircleCI configuration

--- main.tf
+++ main.tf
@@ -12,28 +12,3 @@ provider "aws" {
   region     = "us-east-1"
   version    = "2.47.0"
 }
-
-resource "aws_iam_user" "alxmedium_circleci_iam_user" {
-  name = "alxmedium_circleci"
-  path = "/"
-}
-
-resource "aws_iam_user_policy" "alxmedium_circleci_iam_user_policy" {
-  name   = "alxmedium_circleci"
-  user   = aws_iam_user.alxmedium_circleci_iam_user.name
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "iam:Get*",
-                "iam:PutUserPolicy*"
-            ],
-            "Resource": "*"
-        }
-    ]
-}
-  EOF
-}
